de3134122d777f0caf6a2097a9e50ae5affa2878
Roman Vlasenko <klavionik@gmail.com>
2023-05-13 23:28:28 +0300

    Add Flux img repository notification script

---
+++ scripts/notify-flux-imgrepo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+WEBHOOK_URL="https://webhook.flux.klavionik.net/hook/0bacd89674884e13132c6c41c3e343283d647526c9209206976a29c59ab17d76"
+HASH_FUNCTION=sha256
+REQUEST_BODY=$@
+
+HASH=$(printf "$REQUEST_BODY" | openssl dgst -"$HASH_FUNCTION" -r -hmac $IMGREPO_WEBHOOK_TOKEN | awk '{print $1}')
+curl "$WEBHOOK_URL" -X POST -H "X-Signature: $HASH_FUNCTION=$HASH" -d "$REQUEST_BODY"
