f08c75a702feae00d4ead278722fdca07530fa58
Roman Vlasenko <klavionik@gmail.com>
2023-05-14 19:19:55 +0300

    Put another_secret back

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import os
 app = FastAPI()
 hostname = socket.gethostname()
 secret = os.environ.get("SECRET")
+another_secret = os.environ.get("ANOTHER_SECRET")
 
 
 class Hello(pydantic.BaseModel):
     msg: str
     secret: str
+    another_secret: str
 
 
 @app.get("/hello", response_model=Hello)
 async def hello(name: str):
-    return {"msg": f"Hello {name} from {hostname}!", "secret": secret}
+    return {"msg": f"Hello {name} from {hostname}!", "secret": secret, "another_secret": another_secret}
 
 
 if __name__ == '__main__':
