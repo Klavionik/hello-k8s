2f5c17cac149e548290f87b740085db7f3e2adb3
Roman Vlasenko <klavionik@gmail.com>
2023-05-16 01:07:11 +0300

    Delete k8s manifests

--- scripts/notify-flux-imgrepo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-WEBHOOK_URL="https://webhook.flux.klavionik.net/hook/0bacd89674884e13132c6c41c3e343283d647526c9209206976a29c59ab17d76"
-HASH_FUNCTION=sha256
-REQUEST_BODY=$@
-
-HASH=$(printf "$REQUEST_BODY" | openssl dgst -"$HASH_FUNCTION" -r -hmac $IMGREPO_WEBHOOK_TOKEN | awk '{print $1}')
-wget -q --header "X-Signature: $HASH_FUNCTION=$HASH" --post-data "$REQUEST_BODY" $WEBHOOK_URL -O - > /dev/null
